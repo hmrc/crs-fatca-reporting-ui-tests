b828f93a6835ea95a1a09bb01a8b82d1972d90f2
jamesvarghese701 <113344779+jamesvarghese701@users.noreply.github.com>
2025-07-08 11:20:26 +0100

    crs fatca reporting ui test first commit

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,11 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "ui-test-runner" % "0.45.0" % Test
+    "ch.qos.logback"       % "logback-classic" % "1.5.1"         % Test,
+    "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8"        % Test,
+    "org.scalatest"       %% "scalatest"       % "3.2.18"        % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.45.0"        % Test,
+    "uk.gov.hmrc"         %% "domain"          % "8.3.0-play-28" % Test
   )
 
 }
