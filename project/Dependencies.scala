35bf07cd73dfb89a556e89c1099649e457abc29a
jamesvarghese701 <113344779+jamesvarghese701@users.noreply.github.com>
2025-07-08 11:44:07 +0100

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,11 +3,11 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "ch.qos.logback"       % "logback-classic" % "1.5.1"         % Test,
-    "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8"        % Test,
-    "org.scalatest"       %% "scalatest"       % "3.2.18"        % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.46.0"        % Test,
-    "uk.gov.hmrc"         %% "domain"          % "8.3.0-play-28" % Test
+    "ch.qos.logback"       % "logback-classic" % "1.5.18" % Test,
+    "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8" % Test,
+    "org.scalatest"       %% "scalatest"       % "3.2.19" % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.46.0" % Test,
+    "uk.gov.hmrc"         %% "domain-play-30"  % "12.1.0" % Test
   )
 
 }
