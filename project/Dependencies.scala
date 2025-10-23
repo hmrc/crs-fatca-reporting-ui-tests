15756ba7b3ce989ba1779059e35c141ab9529fd8
jamesvarghese701 <113344779+jamesvarghese701@users.noreply.github.com>
2025-10-23 15:32:47 +0100

    address bot comments

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     "ch.qos.logback"       % "logback-classic"     % "1.5.18" % Test,
     "com.vladsch.flexmark" % "flexmark-all"        % "0.64.8" % Test,
     "org.scalatest"       %% "scalatest"           % "3.2.19" % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner"      % "0.49.0" % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"      % "0.50.0" % Test,
     "uk.gov.hmrc"         %% "domain-test-play-30" % "13.0.0" % Test
   )
 
