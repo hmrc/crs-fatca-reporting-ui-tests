bcc0e8c3b7f7f212463c327e9fcb4e5aa60eb30a
James Varghese <113344779+jamesvarghese701@users.noreply.github.com>
2026-01-29 12:45:57 +0000

    DAC6-3831: Elections-not-sent problem page – dynamic variants (#21)

--- src/test/scala/uk/gov/hmrc/ui/pages/ProblemElectionNotSentPage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/ProblemElectionNotSentPage.scala
@@ -16,8 +16,58 @@
 
 package uk.gov.hmrc.ui.pages
 
+import org.openqa.selenium.By
+import uk.gov.hmrc.selenium.webdriver.Driver
+
 object ProblemElectionNotSentPage extends BasePage {
 
   override val pageUrl: String = baseUrl + "/problem/elections-not-sent"
+
+  private val headingSelector     = By.cssSelector("#main-content h1.govuk-heading-l")
+  private val bodySelector        = By.cssSelector("#main-content p.govuk-body")
+  private val backLinkSelector    = By.cssSelector(".govuk-back-link")
+  private val warningTextSelector = By.cssSelector("#main-content .govuk-warning-text")
+
+  val expectedWarningText: String =
+    "You must finish sending your file to complete the reporting process."
+
+  val commonBodyPhrase: String =
+    "You can still send your file without the elections, then add them after in the service."
+
+  val giinReceivedElectionsNotReceivedPhrase: String =
+    "We have received the GIIN for this financial institution, but have not been able to receive the elections."
+
+  def heading: String =
+    Driver.instance.findElement(headingSelector).getText.trim
+
+  def bodyText: String =
+    Driver.instance.findElement(bodySelector).getText.trim
+
+  def backLinkIsPresent: Boolean =
+    !Driver.instance.findElements(backLinkSelector).isEmpty
+
+  def warningTextPresent: Boolean =
+    !Driver.instance.findElements(warningTextSelector).isEmpty
+
+  def warningText: String =
+    Driver.instance.findElement(warningTextSelector).getText.trim
+
+  def assertPageIsDisplayed(): Unit = {
+    onPage()
+    heading            shouldBe "There is a problem with sending the elections"
+    backLinkIsPresent  shouldBe false
+    warningTextPresent shouldBe true
+    warningText          should include(expectedWarningText)
+  }
+
+  def assertElectionNotSentVariant(): Unit = {
+    assertPageIsDisplayed()
+    bodyText should include(commonBodyPhrase)
+  }
+
+  def assertGiinReceivedElectionsNotSentVariant(): Unit = {
+    assertPageIsDisplayed()
+    bodyText should include(giinReceivedElectionsNotReceivedPhrase)
+  }
 
 }
--- src/test/scala/uk/gov/hmrc/ui/specs/FiReportingProblemElectionNotSentSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/FiReportingProblemElectionNotSentSpec.scala
@@ -24,41 +24,52 @@ class FiReportingProblemElectionNotSentSpec extends BaseSpec {
   Feature("FI Reporting - Upload and problem pages") {
 
     Scenario("CRS file submission shows elections-not-sent problem page", ReportingTests, SoloTests) {
+
+      And("The user logs in as an organisation")
       AuthLoginPage.loginAsOrganisationUser()
       When("The user hits the uploading page and submits a valid XML file")
-
       UploadFilePage
         .onPage()
         .fileUpload("valid-crs-election-not-sent-xml.xml")
 
-      And("They choose to send elections for the reporting period")
+      And("They choose not to send elections for the reporting period")
       ReportElectionsPage.selectNoAndContinue()
 
       Then("They continue from the Check your file details page")
       CheckYourFileDetailsPage.submitPage()
       And("They confirm and send the file")
       SendYourFilePage.submitFileForValidation()
-      Then("The elections-not-sent problem page is displayed")
+      Then("The elections-not-sent problem page is displayed (election not provided)")
       ProblemElectionNotSentPage.onPage()
+      ProblemElectionNotSentPage.assertElectionNotSentVariant()
     }
 
     Scenario("FATCA file submission shows elections-not-sent problem page", ReportingTests, SoloTests) {
+
+      And("The user logs in as an organisation")
       AuthLoginPage.loginAsOrganisationUser()
       When("The user hits the uploading page and submits a valid XML file")
-
       UploadFilePage
         .onPage()
-        .fileUpload("valid-fatca-elections-not-sent-xml.xml")
+        .fileUpload("valid-fatca-election-not-sent-giin-provided.xml")
+
+      And("They enter a GIIN if prompted")
+      RequiredGIINPage.maybeEnterGiin()
 
       And("They choose to send elections for the reporting period")
-      ReportElectionsPage.selectNoAndContinue()
+      ReportElectionsPage.selectYesAndContinue()
+
+      And("They answer the FATCA questions (US treasury regulations and thresholds)")
+      FatcaUSTreasuryRegulationsPage.selectYesAndContinue()
+      FatcaThresholdsPage.selectYesAndContinue()
 
       Then("They continue from the Check your file details page")
       CheckYourFileDetailsPage.submitPage()
       And("They confirm and send the file")
       SendYourFilePage.submitFileForValidation()
-      Then("The elections-not-sent problem page is displayed")
+      Then("The elections-not-sent problem page is displayed (GIIN received, elections not sent)")
       ProblemElectionNotSentPage.onPage()
+      ProblemElectionNotSentPage.assertGiinReceivedElectionsNotSentVariant()
     }
   }
 }
